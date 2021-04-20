dc9e1e055ca677162ee0304e1c4eaf76c81c3c59
beendra <alexandra.ricardel@gmail.com>
2021-04-19 20:33:35 -0500

    a sea of confusion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-    before_action :authenticate, only: [:show, :update]
+    before_action :authenticate, only: [:show, :update, :delete]
 
     def login 
-        user = User.first
+        user = User.find(params[:id])
         render json: user
     end
 
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     end 
 
     def show
-        user = User.find(params[:id])
+        user = User.find_by(params[:id])
         render json: user
     end 
 
@@ -21,13 +21,13 @@ class UsersController < ApplicationController
     end 
 
     def update
-        @user = User.find(params[:id])
+        @user = User.find_by(params[:id])
         @user.update(user_params)
         render json: @user 
     end 
 
     def destroy
-        @user = User.find(params[:id])
+        @user = User.find_by(params[:id])
         @user.destroy
         render json: @user
     end 
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   post "/login", to: "users#login"
   patch "/profile", to: "users#update"
   post "/users", to: "users#create"
+  delete "/users/:id", to: "users#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
